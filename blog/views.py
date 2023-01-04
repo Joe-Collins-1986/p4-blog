4f7c1f14f3f3aef394b52452a4788cfc6289c69a
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-01-04 16:16:13 +0000

    style base with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Joe C',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': '12 Aug 2022'
+    },
+    {
+        'author': 'Lizzy C',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': '14 Aug 2022'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
