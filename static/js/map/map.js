c8400b10a72389596407921ad25c35a3804a69df
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-01-18 15:07:54 +0000

    test js function

--- static/js/map/map.js
+++ static/js/map/map.js
@@ -1,22 +1,14 @@
 
-function myFunction() {
-  alert("Hello from a static file!");
-}
-
 window.onload=function(){
 
-    // selecting the elements for which we want to add a tooltip
-    const target = document.getElementById("target");
-    const prompt = document.getElementById("prompt");
+    const targets = document.querySelectorAll('.target');
+
+    targets.forEach(target => {
+      target.addEventListener('mouseover', function handleHover(event) {
+        console.log('target hovered over', event);
     
-    // change display to 'block' on mouseover
-    target.addEventListener('mouseover', () => {
-      prompt.style.display = 'block';
-    }, false);
-    
-    // change display to 'none' on mouseleave
-    target.addEventListener('mouseleave', () => {
-      prompt.style.display = 'none';
-    }, false);
-    
+        target.setAttribute('style', 'background-color: yellow;');
+      });
+    });
+        
     }
