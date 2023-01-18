2ce34d485ab8662621cc003f98185d0352566219
Joe-Collins-1986 <collins.jw@btinternet.com>
2023-01-18 11:40:54 +0000

    js test button

--- static/js/map/map.js
+++ static/js/map/map.js
@@ -2,3 +2,21 @@
 function myFunction() {
   alert("Hello from a static file!");
 }
+
+window.onload=function(){
+
+    // selecting the elements for which we want to add a tooltip
+    const target = document.getElementById("target");
+    const prompt = document.getElementById("prompt");
+    
+    // change display to 'block' on mouseover
+    target.addEventListener('mouseover', () => {
+      prompt.style.display = 'block';
+    }, false);
+    
+    // change display to 'none' on mouseleave
+    target.addEventListener('mouseleave', () => {
+      prompt.style.display = 'none';
+    }, false);
+    
+    }
